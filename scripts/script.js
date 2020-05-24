8f21667bc7a71e1b65678e2cfd452e858caf7a97
CALauer <56165582+CALauer@users.noreply.github.com>
2020-05-24 16:42:30 -0400

    Revised for more dynamic capability

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,41 @@
-$('#load_data_btn').click(function() {
-	$.ajax({
-		url: 'https://aimtell.com/files/sites.json',
-		type: 'GET',
-		dataType: 'json',
-		success: function(data, status) {
-			//   console.log('Callback Successful')
-			let source = document.querySelector('#pushPros').innerHTML;
-			let template = Handlebars.compile(source);
-			let html = template(data);
-			let destination = document.querySelector('#data_table');
-			destination.innerHTML = html;
-		},
-		error: function(data, status) {
-			console.log('Callback failed');
-		}
-	});
-});
+function getData() { //Gets the data from the JSON URL
+    return $.ajax({
+        url : 'https://aimtell.com/files/sites.json',
+        type: 'GET'
+    });
+}
+getData().done(initialize);
+function initialize (data) {
+	let siteData = 
+	'{{#each sites}}{{#if @last}}{{#each this}}<th class="border-bottom">{{@key}}</th>{{/each}}{{/if}}{{/each}}'
+	let source = document.querySelector('#pushPros').innerHTML
+	let template = Handlebars.compile(siteData)
+	let html = template(data)
+	let destination = document.querySelector('#data1')
+	destination.innerHTML = html
+console.log(data)
+	console.log(data)
+}
+function loadDataTable(data) {
+	 let siteData = 
+	  '{{#each sites}}{{#if @last}}{{#each this}}<th class="border-bottom">{{@key}}</th>{{/each}}{{/if}}{{/each}}'
+	  + '{{#each sites}}<tr><td class="font-weight-bold border-bottom">{{id}}</td>'
+			+ '<td class="border-bottom">{{uid}}</td><td class="border-bottom">'
+			+ '<a id ="show_url"href="{{url}}">Here</a></td>'
+			+ '<td class="border-bottom">{{name}}</td>'
+			+ '<td class="border-bottom"><a href="{{icon}}">View Icon</a></td>'
+			+ '<td class="border-bottom">{{promptId}}</td>'
+			+ '<td class="border-bottom">{{webPushID}}</td>'
+			+ '<td class="border-bottom">{{auto_prompt}}</td>'
+			+ '<td class="border-bottom">{{createdAt}}</td>'
+			+ '<td class="border-bottom">{{active}}</td></tr>{{/each}}'
+      let source = document.querySelector('#pushPros').innerHTML
+      let template = Handlebars.compile(siteData)
+	  let html = template(data)
+      let destination = document.querySelector('#data1')
+      destination.innerHTML = html
+
+}
+$('#load_data_btn').click(function () {
+	getData().done(loadDataTable) // Load Data On Click
+})
